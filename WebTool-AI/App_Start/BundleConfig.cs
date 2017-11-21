9f40e1e68385a6dd8b16de2a2920543ef32364d1
Samantha <samben3793@gmail.com>
2017-11-21 21:41:01 +0400

    commit

--- WebTool-AI/App_Start/BundleConfig.cs
+++ WebTool-AI/App_Start/BundleConfig.cs
@@ -9,7 +9,7 @@ namespace WebTool_AI
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery-1.12.4.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
